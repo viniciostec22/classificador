12242e1cc7aa6ef2162b0da74709a1b235ac23c4
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2024-03-28 13:11:21 -0300

    modified:   templates/partials/header.html modified:   users/urls.

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-   path('new_user/', views.new_user, name="cadastro" ),
+   # path('new_user/', views.new_user, name="cadastro" ),
    path('login/', views.logar, name="login"),
    path('logout/', views.logout, name='logout')
 ]
